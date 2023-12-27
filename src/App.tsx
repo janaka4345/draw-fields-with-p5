f86e1062ee5d4dbc87ad8393c501b418dda23ee7
banuka <banukaramanayaka76@gmail.com>
2023-12-27 19:07:21 +0530

    feat:flow field efficency test with aray.map

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Canvas2 from "./components/Canvas2";//perlin noise graph
 import Canvas3 from "./components/Canvas3";// perlin noise
 import Canvas4 from "./components/Canvas4";//efficient perlin noise
 import Canvas5 from "./components/Canvas5";//flow field with perlin noise
+import Canvas6 from "./components/Canvas6";//flow field with perlin noise try 2
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       {/* <Canvas2/> */}
       {/* <Canvas3/> */}
       {/* <Canvas4/> */}
-      <Canvas5/>
+      {/* <Canvas5/> */}
+      <Canvas6/>
 
 
     </>
--- src/components/Canvas6.jsx
+++ src/components/Canvas6.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { ReactP5Wrapper } from "@p5-wrapper/react";
-// let angleArray = [];
-export default function Canvas5(props) {
+let angleArray = [];
+export default function Canvas6(props) {
   return (
     <div>
       <div
@@ -44,53 +44,58 @@ function draw(p5) {
 
   let coloumns = Math.floor(200 / scale);
   let rows = Math.floor(200 / scale);
+
   return () => {
     p5.background(255, 0, 0);
 
     p5.noFill();
     p5.stroke(0);
 
-    // p5.text(p5.frameCount, 50, 50);
-    // p5.loadPixels();
     let yoff = 0;
-
-    // p5.noStroke();
-    for (let y = 0; y < rows; y++) {
+    [...Array(rows)].map((_, i) => {
       let xoff = 0;
-      for (let x = 0; x < coloumns; x++) {
+      [...Array(coloumns)].map((_, j) => {
         let angle = p5.noise(xoff, yoff, zoff) * p5.TWO_PI;
-        // angleArray.push(angle);
-        let vector = p5.createVector(Math.cos(angle), Math.sin(angle)); //p5.Vector.fromAngle is not working with wrapper
-        p5.push();
 
-        // // // p5.fill(val);
-        // // // p5.square(x * scale, y * scale, scale);
-        p5.translate(x * scale, y * scale);
+        let vector = p5.createVector(Math.cos(angle), Math.sin(angle));
+        angleArray.push(vector);
+        p5.push();
+        p5.translate(j * scale, i * scale);
         p5.rotate(vector.heading());
         p5.line(0, 0, scale, 0);
         p5.pop();
-
-        // let index = (x + y * p5.canvas.width) * 4;
-
-        // let val = p5.noise(xoff, yoff) * 255;
-        // p5.pixels[index] = val;
-        // p5.pixels[index + 1] = val;
-        // p5.pixels[index + 2] = val;
-        // p5.pixels[index + 3] = 255;
         xoff += 0.1;
-      }
+      });
       yoff += 0.1;
-    }
+    });
+
+    // p5.removeElements();
+    // let yoff = 0;
+    // for (let y = 0; y < rows; y++) {
+    //   let xoff = 0;
+    //   for (let x = 0; x < coloumns; x++) {
+    //     let angle = p5.noise(xoff, yoff, zoff) * p5.TWO_PI;
+    //     // let angle = parseFloat((p5.noise(xoff, yoff, zoff) * p5.TWO_PI).toFixed(3));
+    //     let vector = p5.createVector(Math.cos(angle), Math.sin(angle));
+    //     p5.push();
+    //     p5.translate(x * scale, y * scale);
+    //     p5.rotate(vector.heading());
+    //     p5.line(0, 0, scale, 0);
+    //     p5.pop();
+
+    //     xoff += 0.1;
+    //   }
+    //   yoff += 0.1;
+    // }
     p5.push();
     p5.fill(255, 120, 0);
     p5.circle(x, x, 10);
     p5.pop();
     x > 200 ? (x = 0) : x++;
     zoff += 0.1;
-    // p5.updatePixels();
     // p5.noLoop();
   };
 }
 function mousePressed(p5) {
-  console.log(angleArray);
+  console.log(p5.frameRate());
 }
